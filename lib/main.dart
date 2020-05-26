2408200bcbb247228bb5a64d98be0e4c2ce8929b
Shraddha saini <shraddhasaini99@gmail.com>
2020-05-26 18:08:45 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Where is Everyone'),
+      centerTitle: true,
+      backgroundColor: Colors.amber[400],
+    ),
+    body: Center(
+      child: Text(
+          'Hello Username',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.red[600],
+          ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+    ),
+  ),
+));
