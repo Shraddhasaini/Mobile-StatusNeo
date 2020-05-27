dfa0c8bebc60ba59fe60bc17da9e67e729c5ec97
Shraddha saini <shraddhasaini99@gmail.com>
2020-05-27 10:06:37 +0530

    shortcut

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Where is Everyone'),
-      centerTitle: true,
-      backgroundColor: Colors.amber[400],
-    ),
-    body: Center(
-      child: Text(
-          'Hello Username',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.red[600],
-          ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-    ),
-  ),
+  home: Home()
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Where is Everyone'),
+        centerTitle: true,
+        backgroundColor: Colors.amber[400],
+      ),
+      body: Center(
+        child: Image.asset('assets/statusneo_logo.png'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+      ),
+    );
+  }
+}
+
